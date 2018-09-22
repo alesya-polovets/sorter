f0f04b9e283ae688ce7bf7b64d8fcada03d7ecd7
Alesya Polovets <alesya.polovets@gmail.com>
2018-09-22 12:14:43 +0300

    sorter/feature: implemented class Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,51 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.arr = [];
+      this.comparator = function (left, right) {
+        return left - right;
+      }
   }
 
   add(element) {
     // your implementation
+      this.arr.push(element);
+      return this.arr;
   }
 
   at(index) {
     // your implementation
+      return this.arr[index];
   }
 
   get length() {
     // your implementation
+      return this.arr.length;
   }
 
   toArray() {
     // your implementation
+      return this.arr;
   }
 
   sort(indices) {
     // your implementation
+      let result = [];
+      if (indices[0] > indices[indices.length - 1]) {
+        indices.reverse();
+      }
+      for (let i = 0; i < indices.length; i++) {
+        result.push(this.arr[indices[i]]);
+      }
+      result.sort(this.comparator);
+      for (let i = 0; i < indices.length; i++) {
+        this.arr.splice(indices[i], 1, result[i]);
+      }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.comparator=compareFunction;
   }
 }
 
